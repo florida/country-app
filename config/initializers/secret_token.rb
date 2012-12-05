1739f1191a48f7a7d41f293122b0f8dfb9c2f3d1
Florida Elago <florida.elago@gmail.com>
2012-12-05 14:02:19 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CountryApp::Application.config.secret_token = '8edbd5ac2ed4dcfbfc01cddac4a21fcb3a63b55df2f1e94f4a8f8e884bcfa3de326bd1ccc352c003b04024ce23e429ff8d9c88ad45f8a4f7d42ee0e3d76b2649'
